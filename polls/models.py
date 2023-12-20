d68b17a0c2efcea0b2fef49254d58434f60cc71d
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-20 21:49:55 +0530

    rename Choice model choice_test to choice_text

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_test = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     vote = models.IntegerField(default=0)
 
     def __str__(self):
