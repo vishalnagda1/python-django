f1ef59bd47a703850d6e77915d7c14c4406952b0
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2023-12-28 19:38:13 +0530

    update the polls app views

--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def details(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, question_id):
+    return HttpResponse("You're looking at detail of question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at question %s."
+    response = "You're looking at results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
